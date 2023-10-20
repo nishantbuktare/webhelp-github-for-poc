f56895bdf283a68661f6340ca6bb5598735e2d40
nishantbuktare <nishant.buktare@gmail.com>
2023-10-20 13:03:43 +0530

    adding echo

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
+
+echo "Deployment process started..."
+
 cd sample/out
+
+echo "Initializing Git repository..."
 rev=$(git rev-parse --short HEAD)
 git init
-git config user.name ${USER_NAME}
-git config user.email ${USER_MAIL}
-git remote add upstream ${GIT_REMOTE}
+
+echo "Configuring Git user..."
+git config user.name "${USER_NAME}"
+git config user.email "${USER_MAIL}"
+
+echo "Adding and configuring the remote repository..."
+git remote add upstream "${GIT_REMOTE}"
 git fetch upstream
+
+echo "Resetting the repository to the latest commit..."
 git reset upstream/gh-pages
+
+echo "Adding changes to the Git repository..."
 git add .
+
+echo "Committing changes..."
 git commit -m "Rebuild pages at ${rev}"
+
+echo "Pushing changes to the remote repository..."
 git push -q upstream HEAD:gh-pages
+
+echo "Deployment process completed."
