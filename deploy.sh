a996c39c9aebec6b8c93be2b421d3bd3f8baaf3c
nishantbuktare <nishant.buktare@gmail.com>
2023-10-22 21:09:46 +0530

    asdasdsads

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
+
+echo "Changing directory to 'sample/out'"
 cd sample/out
+
+echo "Getting the short Git revision hash"
 rev=$(git rev-parse --short HEAD)
+
+echo "Initializing a new Git repository"
 git init
-git config user.name ${USER_NAME}
-git config user.email ${USER_MAIL}
-git remote add upstream ${GIT_REMOTE}
+git config user.name "nishantbuktare"
+git config user.email "nishant.buktare@gmail.com"
+
+echo "Adding the upstream repository"
+git remote add upstream "https://github.com/nishantbuktare/space-pages.git"
+
+echo "Fetching the upstream repository"
 git fetch upstream
+
+echo "Resetting the repository to upstream's gh-pages branch"
 git reset upstream/gh-pages
+
+echo "Adding all files to the repository"
 git add .
+
+echo "Committing changes with the revision hash"
 git commit -m "Rebuild pages at ${rev}"
+
+echo "Pushing changes to the upstream's gh-pages branch"
 git push -q upstream HEAD:gh-pages
--- publish.sh
+++ publish.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-# Copy the LICENSE_KEY variable into 'licensekey.txt' in Oxygen Publishing Engine 
+
+echo "Copying the LICENSE_KEY variable into 'licensekey.txt' in Oxygen Publishing Engine"
+# Copy the LICENSE_KEY variable into 'licensekey.txt' in Oxygen Publishing Engine
 printf "%s" "$LICENSE_KEY" > ope/licensekey.txt
-# The LICENSE_KEY variable does not contain new line character but '@@' instead
+
+echo "Replacing '@@' with '\\n' in 'licensekey.txt'"
+# The LICENSE_KEY variable does not contain a new line character but '@@' instead
 # Replace all the '@@' characters back to '\n' in the 'licensekey.txt' file
 sed -i 's/@@/\n/g' ope/licensekey.txt
+
+echo "Running the PDF CSS HTML5 transformation"
 # Run the PDF CSS HTML5 transformation
-ope/bin/dita -f pdf-css-html5 -i sample/space.ditamap -o sample/out/pdf-css-html5 -Dpdf.publishing.template=publishing  -Dpdf.publishing.template.descriptor=basket.opt
+ope/bin/dita -f pdf-css-html5 -i sample/space.ditamap -o sample/out/pdf-css-html5 -Dpdf.publishing.template=publishing -Dpdf.publishing.template.descriptor=basket.opt
+
+echo "Running the WebHelp Responsive transformation"
 # Run the WebHelp Responsive transformation
-ope/bin/dita -f webhelp-responsive -i sample/space.ditamap -o sample/out/webhelp-responsive -Dwebhelp.publishing.template=publishing  -Dwebhelp.publishing.template.descriptor=basket.opt
+ope/bin/dita -f webhelp-responsive -i sample/space.ditamap -o sample/out/webhelp-responsive -Dwebhelp.publishing.template=publishing -Dwebhelp.publishing.template.descriptor=basket.opt
